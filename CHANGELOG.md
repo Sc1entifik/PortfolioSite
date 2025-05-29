10852799c9150ccd94e96cd1f2333ea30f37bc60
Scientifik <scientifik1@protonmail.com>
2025-05-29 14:50:13 -0400

    Added Sitemap enum for storing all the page routes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 ##Versions
 - v-0.0.1 - Initial commit. Added CHANGELOG.md modified README.md.
-- v-0.3 - Installed custom font configured for tailwind and added colors.
+- v-0.1 - Installed custom font configured for tailwind and added colors. Added a Sitemap Enum to hold all the routes the website will have.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { SiteMap } from "@/utils/siteMap";
+
 export default function Home() {
 	return (
-		<div className="bg-dos-blue min-h-[100vh] text-center">
-			<div className="flex flex-col font-dos text-dos-white text-[1rem]">
+		<div className="bg-dos-blue min-h-[100vh] flex justify-center">
+			<div className="flex flex-col font-dos text-dos-white text-xl">
 				<h1>The quick brown fox jumps over the lazy dog.</h1>
 				<p>I wonder wtf like really wtf?</p>
+				<p>This always aligns left to right</p>
+				<p>Even though the block is centered</p>
 			</div>
 		</div>
 	);
