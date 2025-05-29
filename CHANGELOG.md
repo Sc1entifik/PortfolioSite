0bce1ca1abd42a8a22ba33d2f1e75ac04d31c6a1
Scientifik <scientifik1@protonmail.com>
2025-05-28 20:52:20 -0400

    Added PressStartP2 font. Added colors for the page.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
 ##Versions
 - v-0.0.1 - Initial commit. Added CHANGELOG.md modified README.md.
+- v-0.3 - Installed custom font configured for tailwind and added colors.
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
+			fontFamily: {
+				dos: ["'Press Start 2P'", "monospace"],
+			},
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+				dos: {
+					blue: "hsl(240, 100%, 34%)",
+					red: "hsl(0, 100%, 50%)",
+					black: "hsl(0, 0%, 0%/)",
+					white: "hsl(0,0%, 100%)",
+					cyan: "hsl(180, 100%, 50%)",
+					lightGray: "hsl(0, 0%, 75%)",
+					green: "hsl(120, 100%, 50%)",
+					yellow: "hsl(60, 100%, 50%)",
+				},
       },
     },
   },
