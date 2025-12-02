546d32e9a35074ad74f9287b03d40973c2f7e5dc
Scientifik <scientifik1@protonmail.com>
2025-12-02 00:00:47 -0500

    Set screen breakpoints then deleted tailwind.config.ts to be fully Tailwind v4 compliant.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MenuOptions from "./components/menuOptions";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col justify-between h-dvh w-full text-[15px] sm:text-[22px] md:text-[27px] lg:text-[31px] overflow-hidden">
+		<div className="flex flex-col justify-between h-dvh w-full text-[.8rem] xs:text-[1rem] sm:text-[1.37rem] md:text-[1.68rem] lg:text-[2rem] overflow-hidden">
 			<LandingPageHeader/>
 			<MenuOptions/>
 			<p className="mb-1">{'>'} Enter Your Selection</p>
