144db1ad55697ff8ba407f69741870eb6b929aa8
Scientifik <scientifik1@protonmail.com>
2025-12-12 15:11:28 -0500

    updated generateEnvFile.ts

--- generateEnvFile.ts
+++ generateEnvFile.ts
@@ -5,6 +5,10 @@ const generateEnvFile = () => {
 	const secretKey = randomBytes(32).toString("base64url");
 	console.log(`JWE_SECRET_KEY=${secretKey}`)
 	console.log("SMTP_ADDRESS=")
+	console.log("SMTP_EMAIL_ADDRESS=")
+	console.log("SMTP_EMAIL_DESTINATION=")
+	console.log("SMTP_PASSWORD=")
+	console.log("SMTP_PORT=")
 }
 
 generateEnvFile();
