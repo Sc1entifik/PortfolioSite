62332a0658fc8bec8d5c9d481f13f6eef02779ef
Scientifik <scientifik1@protonmail.com>
2025-12-14 16:40:28 -0500

    Added Suspense wrapper to CaptchaElements to hopefull make NextJS render the component correctly.

--- src/app/captcha/page.tsx
+++ src/app/captcha/page.tsx
@@ -1,13 +1,16 @@
 import RobotHeader from "./components/robotHeader";
 import HomeLink from "./components/homeLink";
 import CaptchaElements from "./components/captchaElements";
+import { Suspense } from "react";
 
 export default async function Captcha() {
 
 	return (
 		<div className="flex flex-col items-center justify-between h-dvh gap-10 p-2">
 			<RobotHeader/>
-			<CaptchaElements/>
+			<Suspense fallback={<p>Loading...</p>}>
+				<CaptchaElements/>
+			</Suspense>
 			<HomeLink/>
 		</div>
 	)
