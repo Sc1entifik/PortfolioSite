66cffefd03ba4cda778897c5e4c993381785323f
Scientifik <scientifik1@protonmail.com>
2025-12-15 21:32:25 -0500

    Adding a new captcha to push to production to see if it awaits text or I get blocks

--- src/app/captcha/components/captchaElements.tsx
+++ src/app/captcha/components/captchaElements.tsx
@@ -4,13 +4,17 @@ import CaptchaCanvas from "./captchaCanvas";
 import CaptchaForm from "./captchaForm";
 import { connection } from "next/server";
 
+const testString = async () => "Test Value";
+
 export default async function CaptchaElements() {
 	await connection();
+	const test = await testString();
 	const encryptedCaptcha = await generateEncryptedCaptchaText();
 	const captchaText = await decryptCaptcha(encryptedCaptcha);
 
 	return (
 		<div className="flex flex-col items-center gap-3">
+			<CaptchaCanvas captchaText={test}/>
 			<CaptchaCanvas captchaText={captchaText}/>
 			<CaptchaForm encryptedCaptcha={encryptedCaptcha}/>
 		</div>
