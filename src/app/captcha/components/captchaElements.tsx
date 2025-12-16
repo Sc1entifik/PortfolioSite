61c1cc0a020bc391aa50ca99ae9686fbb810f364
Scientifik <scientifik1@protonmail.com>
2025-12-15 22:08:20 -0500

    Fixing typo on last test.

--- src/app/captcha/components/captchaElements.tsx
+++ src/app/captcha/components/captchaElements.tsx
@@ -15,7 +15,7 @@ export default async function CaptchaElements() {
 	return (
 		<div className="flex flex-col items-center gap-3">
 			<p>{test}</p>
-			<p>encryptedCaptcha</p>
+			<p>{encryptedCaptcha}</p>
 			<CaptchaCanvas captchaText={test}/>
 			<CaptchaCanvas captchaText={captchaText}/>
 			<CaptchaForm encryptedCaptcha={encryptedCaptcha}/>
